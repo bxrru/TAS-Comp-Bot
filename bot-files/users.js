ab727665bf40b12c07a5781fc264e1411b39daac
Eddio0141 <31967258+Eddio0141@users.noreply.github.com>
2019-03-14 10:59:10 +0000

    Update users.js

--- bot-files/users.js
+++ bot-files/users.js
@@ -4,5 +4,13 @@ module.exports = {
 	hasCmdAccess:function(member) {
 		var content = fs.readFileSync("./bot-files/users/use_cmds.txt", "utf8");
 		return content.includes(member.id) || content.includes(member.username + "#" + member.discriminator);
+	},
+	// TODO test
+	addCmdAccess:function(member) {
+		fs.appendFile("./bot-files/users/use_cmds.txt", member.username + "#" + member.discriminator, function (err) {
+			if (err)
+				console.log(err);
+  			console.log("added member " + member.username + "#" + member.discriminator);
+		});
 	}
 };
