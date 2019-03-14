9a754537404880391f4b9ca4f48483b35e302ad0
Eddio0141 <31967258+Eddio0141@users.noreply.github.com>
2019-03-14 15:39:26 +0000

    added banned member check / add
    TODO : get return true / false from these add functions

--- bot-files/users.js
+++ bot-files/users.js
@@ -5,12 +5,24 @@ module.exports = {
 		var content = fs.readFileSync("./bot-files/users/use_cmds.txt", "utf8");
 		return content.includes(member.id) || content.includes(member.username + "#" + member.discriminator);
 	},
+	isBanned:function(member) {
+		var content = fs.readFileSync("./bot-files/users/comp_banned.txt", "utf8");
+		return content.includes(member.id) || content.includes(member.username + "#" + member.discriminator);
+	},
 	// TODO test
 	addCmdAccess:function(usernameAndTag) {
 		fs.appendFile("./bot-files/users/use_cmds.txt", usernameAndTag, function (err) {
 			if (err)
 				console.log(err);
-  			console.log("added member " + member.username + "#" + member.discriminator);
+  			console.log("added member " + usernameAndTag);
+		});
+	},
+	// TODO test
+	addBan:function(usernameAndTag) {
+		fs.appendFile("./bot-files/users/comp_banned.txt", usernameAndTag, function (err) {
+			if (err)
+				console.log(err);
+  			console.log("banned member " + usernameAndTag);
 		});
 	}
 };
