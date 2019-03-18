95bcdb089e37f2ca67c7aaea4f6f63fbf633a8ed
Eddio0141 <31967258+Eddio0141@users.noreply.github.com>
2019-03-18 13:48:10 +0000

    ready for testing

--- bot-files/test.js
+++ bot-files/test.js
@@ -1,91 +1,10 @@
-
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-
-process.title = "2botslater";
+process.title = "test";
 const Eris = require("eris");
 log("Starting...");
 
 var date = new Date(); var timestamp = date.getTime();
 
-var bot = new Eris.CommandClient("lol get your own", {}, {
+var bot = new Eris.CommandClient("NTU1NDg5Njc5NDc1MDgxMjI3.D2smAQ.wJYGkGHK5mdC15kEX3_0wThBA7w", {}, {
     description: "the dopest shit you'll ever see in discord",
     owner: "2secslater",
     prefix: "?"
