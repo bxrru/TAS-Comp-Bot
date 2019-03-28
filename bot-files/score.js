9d616432c72fe3b1d404fa2dfb50907a94ed6bb5
MikeXander <40724902+MikeXander@users.noreply.github.com>
2019-03-27 21:09:34 -0400

    Update score.js
    Fine tuning - automatically calculating the score actually works now

--- bot-files/score.js
+++ bot-files/score.js
@@ -1,5 +1,6 @@
 var score = [];
-var Help = require("./score_help.js");
+var setLength = 5;
+var scoreMsg;
 
 module.exports = {
 
@@ -8,19 +9,7 @@ module.exports = {
   alg:function(k,n) {
     x = (1 - (k-1)/n);
     points = 15*x*x*x*x*x*x + 10*x*x*x*x + 5*x*x + 14*x + 6;
-    return points.toFixed(1);
-  },
-
-
-
-  isInt:function(x) {
-    try {
-      int(x);
-    } catch(err) {
-      return false;
-    } finally {
-      return true;
-    }
+    return parseFloat(points.toFixed(1));
   },
 
 
@@ -42,28 +31,11 @@ module.exports = {
       // assumes there is a space between place and name
       name = a[i].split(" ")[1];
 
-      // reduces the string from the right until
-      // it can make an integer for placement
-      place = a[i].trim();
+      place = parseInt(a[i]);
 
-      while (!This.isInt(place)){
-
-        place = place.substr(-1);
-
-        // failsafe incase of extra characters
-        // at the start of the string
-        if (place.length == 0){
-          place = 0;
-          break;
-        }
-      }
-
-      place = int(place);
-
-      points = This.alg(place, participants);
+      points = this.alg(place, participants);
 
       score.push([place, name, points]);
-
 
       // co-op results
       coop = a[i].indexOf("&");
@@ -81,17 +53,38 @@ module.exports = {
 
 
 
+  // the score stored as an array
   getScore:function(){
-    return This.score;
+    return this.score;
+  },
+
+  setScore:function(a){
+    this.score = [];
+    while (a.length > 0){
+      this.score.push(a.shift());
+    }
   },
 
 
 
-  setScore:function(a){
-    This.score = [];
-    while (a.length > 0){
-      score.push(a.pop());
-    }
+  // Number of tasks in each set
+  getSetLength:function(){
+    return this.setLength;
+  },
+
+  setSetLength:function(x){
+    this.setLength = x;
+  },
+
+
+
+  // The bot's stored score message
+  getScoreMsg:function(){
+    return this.scoreMsg;
+  },
+
+  setScoreMsg:function(msg){
+    this.scoreMsg = msg;
   },
 
 
@@ -99,24 +92,24 @@ module.exports = {
   // scores = [int(place), "Name", float(points)]
   addScores:function(a, b){
 
-    if (a.length==0){return b;}
-    if (b.length==0){return a;}
+    if (a === undefined || a.length == 0){return b;}
+    if (b === undefined || b.length == 0){return a;}
 
     var NewScore = [];
     var name = "";
     var points = 0.0;
 
-    for (i=0; i<a.length; i++){
+    for (var i=0; i<a.length; i++){
 
       name = a[i][1];
       points = a[i][2];
 
       // check if the player appears in both
-      for (i=0; i<b.length; i++){
+      for (var j=0; j<b.length; j++){
 
-        if (name.toUpperCase() == b[i][1].toUpperCase()){
-
-          points += b.slice(i,1)[2];
+        if (name.toUpperCase() == b[j][1].toUpperCase()){
+          // remove the duplicate from b as to not check them again
+          points += b.splice(j,1)[0][2];
           break;
         }
       }
@@ -125,7 +118,7 @@ module.exports = {
     }
 
     // any remaining players only in b
-    for (i=0; i<b.length; i++){
+    for (var i=0; i<b.length; i++){
       NewScore.push([0, b[i][1], b[i][2]]);
     }
 
@@ -138,7 +131,7 @@ module.exports = {
   // returns score with proper placement
   sortScore:function(a){
 
-    if (a.length == 0){return [1, "NA", 0.0];}
+    if (a === undefined || a.length == 0){return [];}
     if (a.length == 1){return [1, a[0][1], a[0][2]];}
 
     // sort by descending points
@@ -146,10 +139,10 @@ module.exports = {
 
     a[0][0] = 1;
 
-    for (i=1; i<a.length; i++){
+    for (var i=1; i<a.length; i++){
 
       // same points as previous => same place as previous
-      if (a[i][2] == a[i - 1][2]){
+      if (a[i][2] == a[i-1][2]){
 
         a[i][0] = a[i-1][0];
 
@@ -165,25 +158,34 @@ module.exports = {
 
 
 
-  scoreToMessage:function(a, task){
+  scoreToMessage:function(a, task, set, useHeader){
 
-    var set = ~~((int(task) - 1)/5) + 1; // floor division
+    if (a === undefined || a.length == 0){return "No Results";}
 
-    if (a.length == 0){return "No Results";}
+    var msg = "**";
 
-    var msg = "**__Set " + set.toString() + " Score ";
-    msg += "After Task " + task.toString() + "__\n\n";
+    if (useHeader === undefined){useHeader = true;}
+
+    if (useHeader){
+
+      if (set === undefined){
+        set = ~~((parseInt(task) - 1)/5) + 1; // floor division
+      }
+
+      msg += "__Set " + set.toString() + " Score ";
+      msg += "After Task " + task.toString() + "__\n\n";
+    }
 
     var stopBold = true;
     var place = 0;
     var name = "";
     var points = 0.0;
 
-  for (i=0; i<a.length; i++){
+    for (var i=0; i<a.length; i++){
 
-    place = a[i][0];
-    name = a[i][1];
-    points = a[i][2];
+      place = a[i][0];
+      name = a[i][1];
+      points = a[i][2];
 
       // bold top 3
       if (place>3 && stopBold){
@@ -191,10 +193,10 @@ module.exports = {
         stopBold = false;
       }
 
-      msg += palce.toString();
+      msg += place.toString();
 
       // Checks last character of place => 1st, 2nd, 3rd, 4th...
-      switch (place.toString().substr(place.length - 1)) {
+      switch (place.toString().substr(place.length - 1, 1)) {
         case "1":
           msg += "st";
           break;
@@ -212,7 +214,11 @@ module.exports = {
           break;
       }
 
-      msg += ". " + name + ": " + points.toString() + "\n";
+      msg += ". " + name + ": " + points.toString();
+
+      if (points.toString().substr(-2,1) != "."){msg += ".0";}
+
+      msg += "\n";
 
     }
 
@@ -228,24 +234,41 @@ module.exports = {
 
   updateScore:function(resultsMessage){
 
-    msg = ""
+    var msg = "";
     try {
-      var pastScore = This.getScore();
-  		var results = This.resultsToScore(resultsMessage.split("\n").splice(0,2));
-  		var newScore = This.addScores(pastScore, results);
-  		newScore = This.sortScore(newScore);
-  		This.setScore(newScore);
-      var task = resultsMessage.split("\n")[0].split(" ")[1]; // "Task # Results"
-  		msg = This.scoreToMessage(newScore, task);
 
-    } catch(err) {
+      // "Task # Results"
+      var task = resultsMessage.split("\n")[0].split(" ")[1];
+
+      // reset scores at the start of a new set
+      if ((task-1) % this.getSetLength == 0){this.setScore([]);}
+
+      var pastScore = this.getScore();
+
+      var results_s = resultsMessage.split("\n")
+      results_s.splice(0,2);
+
+    	var results = this.resultsToScore(results_s);
+
+    	var newScore = this.addScores(pastScore, results);
+
+    	newScore = this.sortScore(newScore);
+
+    	this.setScore(newScore);
+
+    	msg = this.scoreToMessage(this.getScore(), task);
+
+    } catch (e) {
+
       msg = "Could not process results";
 
     } finally {
+
       return msg;
     }
-
   },
+
+
 
   processRequest:function(user, action, args){
 
@@ -253,18 +276,26 @@ module.exports = {
 
     switch (action) {
       case "PRINT":
-        var task = int(args.shift());
-        var set = ~~((int(task) - 1)/5) + 1;
-        if (args.length > 0){
-          set = int(args.shift());
-        }
+
+        // optional task
+        var task = 0;
+        if (args.length > 0){parseInt(args.shift());}
+
+        // optional set
+        var set = ~~((parseInt(task) - 1)/5) + 1;
+        if (args.length > 0){set = parseInt(args.shift());}
+
+        msg = this.scoreToMessage(this.getScore(), task, set, false);
 
         break;
 
       case "SET":
+
         break;
 
       case "CLEAR":
+        this.setScore([]);
+        msg = "Score cleared";
         break;
 
       case "CHANGENAME":
@@ -277,9 +308,18 @@ module.exports = {
         break;
 
       case "ADD":
+        var newScore = this.addScores([0, args[0], parseFloat(args[1])], this.getScore());
+        newScore = this.sortScore(newScore);
+        this.setScore(newScore);
+        msg = "Added ``"+args[0]+": "+args[1]+"``";
         break;
 
       case "REMOVE":
+        break;
+
+      case "CHANGESETLENGTH":
+        this.setSetLength(parseInt(args[0]));
+        msg = "Score will reset every " + parseInt(args[0]).toString() + " tasks";
         break;
 
       case "CALCULATE":
@@ -287,9 +327,10 @@ module.exports = {
 
       default:
         msg = "Failed request, action: ``"+action+"`` not recognized. Action must be ";
-        ["PRINT","SET","CLEAR","CHANGENAME","CHANGEPOINTS","COMBINE","ADD","REMOVE","CALCULATE"].forEach(function(a){
+        ["PRINT","SET","CLEAR","CHANGENAME","CHANGEPOINTS","COMBINE","ADD","REMOVE"].forEach(function(a){
           msg += "``"+a+"``, ";
         });
+        msg+="``CALCULATE``";
         break;
 
     }
